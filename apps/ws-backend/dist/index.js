e4c2b438d88956978c8534c5e409f53d86b2f1aa
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-03-26 16:18:01 +0530

    Migrated project to WSL

---
+++ apps/ws-backend/dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const config_1 = require("@repo/backend-common/config");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+    const queryParam = new URLSearchParams(url.split('?')[1]);
+    const token = queryParam.get('token') || "";
+    const decoded = jsonwebtoken_1.default.verify(token, config_1.JWT_SECRET);
+    if (typeof decoded == "string") {
+        ws.close();
+        return;
+    }
+    if (!decoded || !decoded.userId) {
+        ws.close();
+        return;
+    }
+    ws.on('message', function message(data) {
+        ws.send('pong');
+    });
+});
