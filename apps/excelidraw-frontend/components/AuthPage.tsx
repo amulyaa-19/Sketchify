aeb6607c15e296eb37ef2f04809f3397406beb11
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-03-28 00:55:59 +0530

    created a separete frontend folder

---
+++ apps/excelidraw-frontend/components/AuthPage.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export function AuthPage({isSignin}: {
+    isSignin: boolean
+}) {
+    return <div className="w-screen h-screen flex justify-center items-center">
+        <div className="p-6 m-2 bg-white rounded-2xl">
+            <div className="p-2 text-black border-1  rounded">
+                <input type="text" placeholder="Email"></input>
+            </div>
+            <div className="p-2">
+                
+            </div>
+
+            <div className="pt-2">
+                <button className="bg-red-400 rounded p-2" onClick={() => {
+
+                }}>{isSignin ? "Sign in" : "Sign up"}</button>
+            </div>
+        </div>
+    </div>
+
+}
